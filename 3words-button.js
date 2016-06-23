f3de30ba3fd1c06b14b4af416bf062a641c71d49
David Piesse <piesse@gmail.com>
2016-06-23 10:43:58 +0100

    Change to V2 w3w API

--- 3words-button.js
+++ 3words-button.js
@@ -1,15 +1,15 @@
+//require jQuery & HTML5 navigator functionality
 $('button').on('click', function (e) {
     var t = $('span');
     t.text('Loading...');
     
     navigator.geolocation.getCurrentPosition(function (location) {
         data = {
-            'key':		'insertAPIkey',
-            'position':	location.coords.latitude + ',' + location.coords.longitude
+            'key': 'insertAPIkey',
+            'coords':	location.coords.latitude + ',' + location.coords.longitude
         };
-        
-        $.post('http://api.what3words.com/position', data, function(response) {
-            t.text(response.words.join('.'));
+        $.post('https://api.what3words.com/v2/reverse', data, function(response) {
+            t.text(response.words);
         });
     });  
 });
